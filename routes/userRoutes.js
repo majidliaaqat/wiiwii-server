11282a5404d9ad2c9b7fcb5aad89f1a96e294a44
Majid Liaqat <majidliaaqat@gmail.com>
2023-10-21 00:49:29 +0300

    update password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,14 @@
 const router = require("express").Router();
-const controller = require("../controllers/UserController");
+const controller = require("../controllers/userController");
 const middleware = require("../middleware");
 
 router.post("/register", controller.Register);
 router.post("/login", controller.Login);
+router.put(
+  "/update/:user_id",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdatePassword
+);
 
 module.exports = router;
