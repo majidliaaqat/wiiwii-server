34f1351c62abc58ef114347f410f890f670c9a71
Majid Liaqat <majidliaaqat@gmail.com>
2023-10-25 09:31:20 +0300

    user routes

---
+++ routes/messageRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const messageController = require("../controllers/messageController");
+
+// Create a new message/question
+router.post("/message", messageController.message_create);
+
+// Read a message by ID
+router.get("/fetchmsg/:id", messageController.message_read);
+
+// Update a message by ID
+router.put("/message/:id", messageController.message_update);
+
+// Delete a message by ID
+router.delete("/message/:id", messageController.message_delete);
+
+module.exports = router;
